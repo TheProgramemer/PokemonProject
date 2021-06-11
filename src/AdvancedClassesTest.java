daee333ce00f89ba70262495a6d4d461703865a2
Steven Anderson <stevean@cs.washington.edu>
2021-06-11 15:05:35 -0700

    Switch Statements completed code
    Completed Switch Statement video code/start of refactor code

--- src/AdvancedClassesTest.java
+++ src/AdvancedClassesTest.java
@@ -1,5 +1,7 @@
 public class AdvancedClassesTest {
     public static void main(String[] args) {
-
+        Eevee eevee = new Eevee();
+        Pokemon evolved = eevee.evolve(EvolutionStones.WATER_STONE);
+        evolved.sayName();
     }
 }
--- src/Eevee.java
+++ src/Eevee.java
@@ -6,49 +6,58 @@ public class Eevee extends Pokemon implements StoneEvolvable {
 
     @Override
     public Pokemon evolve(EvolutionStones stone) {
-        if (stone.equals(EvolutionStones.WATER_STONE)) {
-            return new Vaporeon.Builder()
-                    .name("Vaporeon")
-                    .level(this.getLevel())
-                    .build();
-        } else if (stone.equals(EvolutionStones.FIRE_STONE)) {
-            return new Flareon.Builder()
-                    .name("Flareon")
-                    .level(this.getLevel())
-                    .build();
-        } else if (stone.equals(EvolutionStones.THUNDER_STONE)) {
-            return new Jolteon.Builder()
-                    .name("Jolteon")
-                    .level(this.getLevel())
-                    .build();
-        } else if (stone.equals(EvolutionStones.PSYCHIC_STONE)) {
-            return new Espeon.Builder()
-                    .name("Espeon")
-                    .level(this.getLevel())
-                    .build();
-        } else if (stone.equals(EvolutionStones.DARK_STONE)) {
-            return new Umbreon.Builder()
-                    .name("Umbreon")
-                    .level(this.getLevel())
-                    .build();
-        } else if (stone.equals(EvolutionStones.ICE_STONE)) {
-            return new Glaceon.Builder()
-                    .name("Glaceon")
-                    .level(this.getLevel())
-                    .build();
-        } else if (stone.equals(EvolutionStones.LEAF_STONE)) {
-            return new Leafeon.Builder()
-                    .name("Leafeon")
-                    .level(this.getLevel())
-                    .build();
-        } else if (stone.equals(EvolutionStones.FAIRY_STONE)) {
-            return new Sylveon.Builder()
-                    .name("Sylveon")
-                    .level(this.getLevel())
-                    .build();
-        } else {
-            return this;
+        Pokemon evolution = this;
+        switch (stone) {
+            case WATER_STONE:
+                evolution = new Vaporeon.Builder()
+                        .name("Vaporeon")
+                        .level(this.getLevel())
+                        .build();
+                break;
+            case FIRE_STONE:
+                evolution =  new Flareon.Builder()
+                        .name("Flareon")
+                        .level(this.getLevel())
+                        .build();
+                break;
+            case THUNDER_STONE:
+                evolution =  new Jolteon.Builder()
+                        .name("Jolteon")
+                        .level(this.getLevel())
+                        .build();
+                break;
+            case PSYCHIC_STONE:
+                evolution =  new Espeon.Builder()
+                        .name("Espeon")
+                        .level(this.getLevel())
+                        .build();
+                break;
+            case DARK_STONE:
+                evolution =  new Umbreon.Builder()
+                        .name("Umbreon")
+                        .level(this.getLevel())
+                        .build();
+                break;
+            case ICE_STONE:
+                evolution =  new Glaceon.Builder()
+                        .name("Glaceon")
+                        .level(this.getLevel())
+                        .build();
+                break;
+            case LEAF_STONE:
+                evolution =  new Leafeon.Builder()
+                        .name("Leafeon")
+                        .level(this.getLevel())
+                        .build();
+                break;
+            case FAIRY_STONE:
+                evolution =  new Sylveon.Builder()
+                        .name("Sylveon")
+                        .level(this.getLevel())
+                        .build();
+                break;
         }
+        return evolution;
     }
 
     public static class Builder extends Pokemon.Builder {
