3dc8fc4d81eeb8913bbf93f76d5ab871b46a91b8
Steven Anderson <stevean@cs.washington.edu>
2021-06-26 10:10:07 -0700

    ArrayLists Pokedex work completed

--- src/PokemonTest.java
+++ src/PokemonTest.java
@@ -1,17 +1,21 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PokemonTest {
-    public static void readFile(String fileLocation) {
+    public static List<String> readFile(String fileLocation) {
+        List<String> pokedexEntries = new ArrayList<>();
         try {
             File file = new File(fileLocation);
             Scanner fileReader = new Scanner(file);
             while (fileReader.hasNext()) {
-                System.out.println(fileReader.nextLine());
+                pokedexEntries.add(fileReader.nextLine());
             }
         } catch (Exception e) {
             System.out.println("Ruh roh Shaggy");
         }
+        return pokedexEntries;
     }
 
     public static void main(String[] args) {
@@ -19,10 +23,35 @@ public class PokemonTest {
         String johtoDexLocation = "src/Pokedexes/Johto Dex.txt";
         String hoennDexLocation = "src/Pokedexes/Hoenn Dex.txt";
 
-        readFile(kantoDexLocation);
+        List<String> nationalDex = new ArrayList<>();
 
-        readFile(johtoDexLocation);
+        List<String> regionalDex = readFile(kantoDexLocation);
+        nationalDex.addAll(regionalDex);
 
-        readFile(hoennDexLocation);
+        nationalDex.addAll(readFile(johtoDexLocation));
+
+        nationalDex.addAll(readFile(hoennDexLocation));
+
+        for (String entry : nationalDex) {
+            System.out.println(entry);
+        }
+
+        System.out.println(nationalDex.size());
+
+        for (int i = 1; i < 386; i++) {
+            String pokemon = nationalDex.get(i - 1);
+            if (pokemon.contains(String.valueOf(i))) {
+                continue;
+            }
+            System.out.println("Missing pokemon with Dex number " + i);
+            break;
+        }
+
+        if (!nationalDex.contains("359 absol")) {
+            System.out.println("Yep, Absol is missing");
+        }
+
+        nationalDex.add(358, "359 absol");
+        System.out.println(nationalDex.size());
     }
 }
