196e8751afa7ec74eedd7b57d15d8245c4825667
TheProgramemer <stand3rs0n@gmail.com>
2021-07-08 09:49:45 -0700

    Setup for lesson on Stacks

--- src/PokemonTest.java
+++ src/PokemonTest.java
@@ -28,31 +28,7 @@ public class PokemonTest {
 
         List<String> regionalDex = readFile(kantoDexLocation);
         nationalDex.addAll(regionalDex);
-
         nationalDex.addAll(readFile(johtoDexLocation));
-
         nationalDex.addAll(readFile(hoennDexLocation));
-
-        for (String entry : nationalDex) {
-            System.out.println(entry);
-        }
-
-        System.out.println(nationalDex.size());
-
-        for (int i = 1; i < 386; i++) {
-            String pokemon = nationalDex.get(i - 1);
-            if (pokemon.contains(String.valueOf(i))) {
-                continue;
-            }
-            System.out.println("Missing pokemon with Dex number " + i);
-            break;
-        }
-
-        if (!nationalDex.contains("359 absol")) {
-            System.out.println("Yep, Absol is missing");
-        }
-
-        nationalDex.add(358, "359 absol");
-        System.out.println(nationalDex.size());
     }
 }
